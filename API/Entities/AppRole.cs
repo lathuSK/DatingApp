486bb3efc1609377d628a2d1ab319a254dec46ae
lathuSK <lathusansiva@gmail.com>
2024-08-30 13:30:19 +0530

    End of the project

---
+++ API/Entities/AppRole.cs
@@ -0,0 +1,9 @@
+using API.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace API;
+
+public class AppRole : IdentityRole<int>
+{
+    public ICollection<AppUserRole> UserRoles { get; set; } = [];
+}
