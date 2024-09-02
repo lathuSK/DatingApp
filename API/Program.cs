02764f8833f92ecfa42496b8672597c60cc43792
lathuSK <lathusansiva@gmail.com>
2024-09-02 15:04:05 +0530

    after azure

--- API/Program.cs
+++ API/Program.cs
@@ -16,19 +16,32 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
-    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+// Configure CORS
+app.UseCors(policy =>
+    policy.AllowAnyHeader()
+          .AllowAnyMethod()
+          .AllowCredentials()
+          .WithOrigins(
+              "http://localhost:4200",
+              "https://localhost:4200",
+              "https://dating-app-by-lathusk-gxczdqgxftbbbxfu.southindia-01.azurewebsites.net/"
+          )
+);
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
+app.UseDefaultFiles();  // Serve default files (e.g., index.html)
+app.UseStaticFiles();   // Serve static files (e.g., JS, CSS)
 
+// Configure routing
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
-app.MapFallbackToController("Index", "Fallback");
+
+// Fallback to Angular app for client-side routing
+app.MapFallbackToFile("index.html");  // Use MapFallbackToFile for static files
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
